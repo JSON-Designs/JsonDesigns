9f64d6e563529d5718d10891efcff869665d8f58
Froodric <fredricjohansson294@gmail.com>
2020-12-26 15:10:50 +0100

    ADDED MOBILE SUPPORT

--- js/script.js
+++ js/script.js
@@ -41,3 +41,35 @@ for (var i = 0; i < btnContainer.length; i++) {
     this.className += " active";
   });
 }
+
+var IsOpen = false;
+var navbar = document
+  .getElementById("navbar-links")
+  .getElementsByTagName("ul")[0];
+var navbarBackground = document.getElementById("nav-background");
+
+function open_nav() {
+  navbar.style.left = "40px";
+  navbarBackground.style.left = "0px";
+  IsOpen = true;
+}
+
+function close_nav() {
+  navbar.style.left = "-150px";
+  navbarBackground.style.left = "-250px";
+  IsOpen = false;
+}
+
+var btn = document.getElementById("navbutton");
+
+btn.addEventListener(
+  "click",
+  function () {
+    if (IsOpen) {
+      close_nav();
+    } else {
+      open_nav();
+    }
+  },
+  false
+);
