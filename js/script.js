fc027a259a68093dcd0ca7bdb2cea5ddda78dd6c
Froodric <fredricjohansson294@gmail.com>
2021-01-01 23:09:21 +0100

    Redesigned services page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-function infoDesign() {
-  document.getElementById("service").style.display = "none";
-  document.getElementById("information").style.display = "block ";
-  document.getElementById("title").innerHTML = "Design Website";
-}
-
-function infoCode() {
-  document.getElementById("service").style.display = "none";
-  document.getElementById("information").style.display = "block ";
-  document.getElementById("title").innerHTML = "Website Coded.";
-  document.getElementById("item2").innerHTML = "Source Files.";
-  document.getElementById("item3").innerHTML = "Website Designed.";
-  document.getElementById("item4").innerHTML = "Hosting.";
-  document.getElementById("item1").innerHTML = "Website Coded in HTML and CSS.";
-}
-
-function infoCodeDesign() {
-  document.getElementById("service").style.display = "none";
-  document.getElementById("information").style.display = "block ";
-  document.getElementById("title").innerHTML = "Design & Code Website";
-  document.getElementById("price").innerHTML = "20$/HOUR";
-  document.getElementById("item1").innerHTML = "Website Designed & Coded.";
-  document.getElementById("item2").innerHTML = "Source Files.";
-  document.getElementById("item3").innerHTML = "";
-  document.getElementById("item4").innerHTML = "Hosting.";
-}
-
 var btnContainer = document.getElementsByClassName("link");
 
 // Loop through the Li elements and add the active class to the current/clicked Li element
